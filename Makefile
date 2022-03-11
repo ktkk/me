235c5e271657ed432cbcdee714720a2f48e90234
ktkk <dezuttereluka@gmail.com>
2022-03-11 19:44:38 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+SASS=sass
+TSC=tsc
+
+SCRIPT_DIR=scripts
+STYLES_DIR=styles
+
+SCRIPT_OUT_DIR=$(SCRIPT_DIR)/out
+STYLES_OUT_DIR=$(STYLES_DIR)/out
+
+SCRIPT_SRCS=index.ts
+STYLES_SRCS=style.scss
+
+SCRIPT_OUTPUT=$(SCRIPT_SRCS:%.ts=%.js)
+STYLES_OUTPUT=$(STYLES_SRCS:%.scss=%.css)
+
+all: $(SCRIPT_OUT_DIR)/$(SCRIPT_OUTPUT) $(STYLES_OUT_DIR)/$(STYLES_OUTPUT)
+
+$(SCRIPT_OUT_DIR)/$(SCRIPT_OUTPUT): $(SCRIPT_DIR)/$(SCRIPT_SRCS)
+	$(TSC) $< --out $@
+
+$(STYLES_OUT_DIR)/$(STYLES_OUTPUT): $(STYLES_DIR)/$(STYLES_SRCS)
+	$(SASS) $< $@
+
+run: all
+	./run.py
+
+clean:
+	rm -rf $(SCRIPT_OUT_DIR) $(STYLES_OUT_DIR)
---
+++ scripts/index.ts
@@ -0,0 +1,67 @@
+//import $ from "jquery";
+
+//const window_wrapper: HTMLElement = document.querySelector(".window")!;
+//
+//function onDrag(e: MouseEvent) {
+//	var get_style: CSSStyleDeclaration = window.getComputedStyle(window_wrapper);
+//
+//	var left: number = parseInt(get_style.left);
+//	var top: number = parseInt(get_style.top);
+//
+//	window_wrapper.style.left = `${left + e.movementX}px`;
+//	window_wrapper.style.top = `${top + e.movementY}px`;
+//
+//	e.preventDefault();
+//
+//	console.log(e.movementX, e.movementY);
+//}
+//
+//window_wrapper.addEventListener("mousedown", () => {
+//	window_wrapper.classList.add("active");
+//	window_wrapper.addEventListener("mousemove", onDrag);
+//});
+//
+//document.addEventListener("mouseup", () => {
+//	window_wrapper.classList.remove("active");
+//	window_wrapper.removeEventListener("mousemove", onDrag);
+//});
+
+drag_element(document.querySelector(".window")!);
+
+function drag_element(elem: HTMLElement) {
+	var pos1: number = 0, pos2: number = 0, pos3: number = 0, pos4: number = 0;
+
+	elem.onmousedown = drag_mouse_down;
+
+	function drag_mouse_down(e: MouseEvent) {
+		e = e || window.event as MouseEvent;
+
+		e.preventDefault();
+
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+
+		document.onmouseup = close_drag_element;
+		document.onmousemove = element_drag;
+	}
+
+	function element_drag(e: MouseEvent) {
+		e = e || window.event as MouseEvent;
+
+		e.preventDefault();
+
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+
+		elem.style.top = `${elem.offsetTop - pos2}px`;
+		elem.style.left = `${elem.offsetLeft - pos1}px`;
+	}
+
+	function close_drag_element() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
