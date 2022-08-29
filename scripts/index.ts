e727464b0f7a0d8fbb8b04c1639e329a8267e141
ktkk <dezuttereluka@gmail.com>
2022-08-29 20:37:08 +0200

    use motif

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,18 +1,23 @@
-import * as wasm from "./wasm.js"
+//import * as wasm from "./wasm.js"
 
-await wasm.init();
+//await wasm.init();
 
-console.log(wasm.add(1, 2));
-console.log(wasm.sub(2, 1));
+//console.log(wasm.add(1, 2));
+//console.log(wasm.sub(2, 1));
 
-drag_element(document.querySelector(".window")!);
+//drag_element(document.querySelector("#quit-popup")!);
+drag_element(document.querySelector(".titlebar-title")!);
 
 function drag_element(elem: HTMLElement) {
+	console.log(elem);
 	var pos1: number = 0, pos2: number = 0, pos3: number = 0, pos4: number = 0;
 
 	elem.onmousedown = drag_mouse_down;
 
+	elem = elem.closest(".window");
+
 	function drag_mouse_down(e: MouseEvent) {
+		console.log("mouse down");
 		e = e || window.event as MouseEvent;
 
 		e.preventDefault();
@@ -20,11 +25,14 @@ function drag_element(elem: HTMLElement) {
 		pos3 = e.clientX;
 		pos4 = e.clientY;
 
+		console.log(`pos3: ${pos3}, pos4: ${pos4}`);
+
 		document.onmouseup = close_drag_element;
 		document.onmousemove = element_drag;
 	}
 
 	function element_drag(e: MouseEvent) {
+		console.log("mouse move");
 		e = e || window.event as MouseEvent;
 
 		e.preventDefault();
@@ -35,11 +43,16 @@ function drag_element(elem: HTMLElement) {
 		pos3 = e.clientX;
 		pos4 = e.clientY;
 
+		console.log(`pos1: ${pos1}, pos2: ${pos2}, pos3: ${pos3}, pos4: ${pos4}`);
+		console.log(`top: ${elem.offsetTop - pos2}, left: ${elem.offsetLeft - pos1}`);
+
 		elem.style.top = `${elem.offsetTop - pos2}px`;
 		elem.style.left = `${elem.offsetLeft - pos1}px`;
 	}
 
 	function close_drag_element() {
+		console.log("mouse up");
+
 		document.onmouseup = null;
 		document.onmousemove = null;
 	}
