7a5120bc620f1add949101b8cd30ed5f3810284a
ktkk <dezuttereluka@gmail.com>
2022-11-11 17:39:11 +0100

    Add nullability check on HTMLElement

--- scripts/index.ts
+++ scripts/index.ts
@@ -5,66 +5,66 @@ console.log(wasm.add(1, 2));
 console.log(wasm.sub(2, 1));
 
 type Position = {
-	x: number,
-	y: number,
+    x: number,
+    y: number,
 };
 
 const dragElement = async (elem: HTMLElement) => {
-	console.log(elem);
-	type PositionUpdate = {
-		oldPosition: Position,
-		newPosition: Position,
-	};
-	let position: PositionUpdate = {
-		oldPosition: { x: 0, y: 0 },
-		newPosition: { x: 0, y: 0 },
-	};
-	
-	elem.onmousedown = function (e: MouseEvent) {
-		console.log("mouse down");
-		e = e || window.event as MouseEvent;
-		
-		//e.preventDefault();
-		//e.stopPropagation();
-		
-		position.oldPosition = {
-			x: e.clientX,
-			y: e.clientY
-		};
-		
-		console.log(position.oldPosition);
-		
-		document.onmouseup = function () {
-			console.log("mouse up");
-			
-			document.onmouseup = null;
-			document.onmousemove = null;
-		};
-		document.onmousemove = function (e: MouseEvent) {
-			console.log("mouse move");
-			e = e || window.event as MouseEvent;
-			
-			//e.preventDefault();
-			//e.stopPropagation();
-			
-			position.newPosition = {
-				x: position.oldPosition.x - e.clientX,
-				y: position.oldPosition.y - e.clientY
-			};
-			position.oldPosition = {
-				x: e.clientX,
-				y: e.clientY
-			};
-			
-			console.log(position);
-			console.log(`top: ${elem.offsetTop - position.newPosition.y}, left: ${elem.offsetLeft - position.newPosition.x}`);
-			
-			elem.style.top = `${elem.offsetTop - position.newPosition.y}px`;
-			elem.style.left = `${elem.offsetLeft - position.newPosition.x}px`;
-		};
-	};
-	
-	elem = elem.closest(".window");
+    console.log(elem);
+    type PositionUpdate = {
+        oldPosition: Position,
+        newPosition: Position,
+    };
+    let position: PositionUpdate = {
+        oldPosition: { x: 0, y: 0 },
+        newPosition: { x: 0, y: 0 },
+    };
+
+    elem.onmousedown = function(e: MouseEvent) {
+        console.log("mouse down");
+        e = e || window.event as MouseEvent;
+
+        //e.preventDefault();
+        //e.stopPropagation();
+
+        position.oldPosition = {
+            x: e.clientX,
+            y: e.clientY
+        };
+
+        console.log(position.oldPosition);
+
+        document.onmouseup = function() {
+            console.log("mouse up");
+
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+        document.onmousemove = function(e: MouseEvent) {
+            console.log("mouse move");
+            e = e || window.event as MouseEvent;
+
+            //e.preventDefault();
+            //e.stopPropagation();
+
+            position.newPosition = {
+                x: position.oldPosition.x - e.clientX,
+                y: position.oldPosition.y - e.clientY
+            };
+            position.oldPosition = {
+                x: e.clientX,
+                y: e.clientY
+            };
+
+            console.log(position);
+            console.log(`top: ${elem.offsetTop - position.newPosition.y}, left: ${elem.offsetLeft - position.newPosition.x}`);
+
+            elem.style.top = `${elem.offsetTop - position.newPosition.y}px`;
+            elem.style.left = `${elem.offsetLeft - position.newPosition.x}px`;
+        };
+    };
+
+    elem = elem.closest(".window")!;
 };
 
 dragElement(document.querySelector("#quit-popup > .titlebar > .titlebar-title")!);
